7d9985260993f2d932f5f14edda05298edb704c1
David Zaslavsky <diazona@ellipsix.net>
2010-06-21 22:22:06 -0400

    Adding templating action for Python string.Template

--- modulo/templating/__init__.py
+++ modulo/templating/__init__.py
@@ -1,5 +1,19 @@
-# -*- coding: iso-8859-1 -*-
+# -*- coding: utf-8 -*-
+
+from string import Template
+
+from modulo.actions import Action
 
 class EmptyTemplateError(Exception):
     '''An exception to be raised when a template object is empty and produces no output.'''
     pass
+
+class PythonTemplate(Action):
+    namespace='*'
+    @classmethod
+    def derive(cls, template, **kwargs):
+        if isinstance(template, basestring):
+            template = Template(template)
+        return super(PythonTemplate, cls).derive(template=template)
+    def generate(self, rsp, **kwargs):
+        rsp.data = self.template.substitute(kwargs)
