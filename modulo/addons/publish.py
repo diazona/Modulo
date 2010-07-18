9d394851fab77820275591601fa298305623b64b
David Zaslavsky <diazona@ellipsix.net>
2010-07-18 19:03:46 -0400

    Move attachment/upload stuff to separate module

--- modulo/addons/publish.py
+++ modulo/addons/publish.py
@@ -6,9 +6,8 @@ import datetime
 import httplib
 import logging
 import modulo.database
-import os, os.path
-import random
 import re
+import sys
 import urlparse
 from elixir import session, using_options
 from elixir import Boolean, DateTime, Entity, Field, ManyToOne, ManyToMany, OneToMany, String, Unicode, UnicodeText
@@ -51,7 +50,8 @@ class Post(BaseComment):
     summary = Field(UnicodeText)
 
     tags = ManyToMany('Tag')
-    attachments = ManyToMany('Attachment')
+    if 'modulo.addons.upload' in sys.modules:
+        attachments = ManyToMany('Upload')
 
 class EditablePost(Post):
     using_options(inheritance='multi')
@@ -72,10 +72,6 @@ class Comment(BaseComment):
     # can be attached to generic Entities, not just those that inherit from BaseComment.
     # This will require some sort of polymorphism, possibly the AssociationProxy pattern.
 
-class Attachment(Entity):
-    mime_type = Field(String(44)) # the MIME type
-    filename = Field(String(124)) # the file on disk that holds the attachment's content
-
 #---------------------------------------------------------------------------
 # General stuff
 #---------------------------------------------------------------------------
@@ -93,7 +89,7 @@ class TransactionID(Action):
 
 class PostSubmitAggregator(Action):
     editable = False
-    def generate(self, rsp, user, title, text_src, tags=list(), draft=False, category=None, markup_mode=None, summary_src=None, attachments=list(), id=None):
+    def generate(self, rsp, user, title, text_src, tags=list(), draft=False, category=None, markup_mode=None, summary_src=None, uploads=list(), id=None):
         editing = False
         if self.editable:
             if id:
@@ -125,7 +121,7 @@ class PostSubmitAggregator(Action):
                 post.summary_src = summary_src
                 post.text_src = text_src
             post.user = user
-            post.attachments = attachments
+            post.attachments = uploads
         return compact('post')
 
 class PostMarkupParser(Action):
@@ -310,50 +306,3 @@ class LinkbackAutodiscovery(Action):
     def generate(self, rsp, post):
         if not post.draft:
             LinkbackAutodiscoveryParser(post.title, self.blog_name).feed(post.text)
-
-#---------------------------------------------------------------------------
-# Attachments
-#---------------------------------------------------------------------------
-
-def gen_chars(n):
-    '''Produces a set of n random word characters'''
-    return "".join(random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_') for i in xrange(n))
-
-def make_file(suggested):
-    import settings
-    name = suggested and secure_filename(suggested) or gen_chars(16) # intentional non-use of ...if...else...
-    subdirs = os.listdir(settings.upload_dir)
-    random.shuffle(subdirs)
-    # potential for race conditions here, but they're unlikely... ignore for now
-    for subdir in subdirs:
-        path = os.path.join(settings.upload_dir, subdir, name)
-        try:
-            fd = os.open(path, os.O_CREAT | os.O_EXCL | os.O_WRONLY, 0644)
-        except OSError:
-            pass
-        else:
-            file_obj = os.fdopen(fd, 'w')
-            break
-    else: # omg it's actually useful here
-        subdir = gen_chars(4)
-        # Possible, but very unlikely, that this could fail
-        os.mkdir(os.path.join(settings.upload_dir, subdir))
-        path = os.path.join(settings.upload_dir, subdir, name)
-        fd = os.open(path, os.O_CREAT | os.O_EXCL | os.O_WRONLY, 0644)
-        file_obj = os.fdopen(fd, 'w')
-    return file_obj, subdir, name, path
-
-class AttachmentSubmitAggregator(Action):
-    @classmethod
-    def handles(cls, req, params):
-        return len(req.files) > 0
-    def generate(self, rsp):
-        attachments = []
-        for name, upload in self.req.files.iteritems():
-            if upload.filename is None:
-                continue
-            file_obj, subdir, name, path = make_file(upload.filename)
-            with file_obj:
-                upload.save(file_obj)
-            attachments.append(Attachment(mime_type=upload.content_type, filename=path))
-        return compact('attachments')
---
+++ modulo/addons/upload.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+import os, os.path
+import random
+import sys
+from elixir import Entity, Field, String, ManyToMany
+from modulo.actions import Action
+from modulo.utilities import compact
+from werkzeug import secure_filename
+
+#---------------------------------------------------------------------------
+# Database models
+#---------------------------------------------------------------------------
+
+class Upload(Entity):
+    '''Stores metadata relating to an uploaded file.'''
+    mime_type = Field(String(44)) # the MIME type
+    group = Field(String(20)) # the group
+    filename = Field(String(128)) # the name by which the upload should be referenced
+    path = Field(String(256)) # the actual path to the disk file representing this upload
+
+if 'modulo.addons.publish' in sys.modules:
+    sys.modules['modulo.addons.publish'].Post.attachments = ManyToMany('Upload')
+
+#---------------------------------------------------------------------------
+# Functions
+#---------------------------------------------------------------------------
+
+def gen_chars(n):
+    '''Produces a set of n random word characters'''
+    return "".join(random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_') for i in xrange(n))
+
+def make_file(suggested):
+    '''Creates a file in the upload directory based on the suggested name.'''
+    import settings
+    name = suggested and secure_filename(suggested) or gen_chars(16) # intentional non-use of ...if...else...
+    subdirs = os.listdir(settings.upload_dir)
+    random.shuffle(subdirs)
+    # potential for race conditions here, but they're unlikely... ignore for now
+    for subdir in subdirs:
+        path = os.path.join(settings.upload_dir, subdir, name)
+        try:
+            fd = os.open(path, os.O_CREAT | os.O_EXCL | os.O_WRONLY, 0644)
+        except OSError:
+            pass
+        else:
+            file_obj = os.fdopen(fd, 'w')
+            break
+    else: # omg it's actually useful here
+        subdir = gen_chars(4)
+        # Possible, but very unlikely, that this could fail due to the file existing
+        os.mkdir(os.path.join(settings.upload_dir, subdir))
+        path = os.path.join(settings.upload_dir, subdir, name)
+        fd = os.open(path, os.O_CREAT | os.O_EXCL | os.O_WRONLY, 0644)
+        file_obj = os.fdopen(fd, 'w')
+    return file_obj, subdir, name, path
+
+class UploadSubmitAggregator(Action):
+    '''Saves uploaded files'''
+    # TODO: specify ability to handle only certain form fields
+    @classmethod
+    def handles(cls, req, params):
+        return len(req.files) > 0
+    def generate(self, rsp):
+        files_to_save = ((make_file(upload.filename), upload) for name, upload in self.req.files.iteritems() if upload.filename)
+        uploads = []
+        for (file_obj, subdir, name, path), upload in files_to_save:
+            try:
+                upload.save(file_obj)
+            except (IOError,OSError):
+                logging.getLogger('modulo.addons.upload').exception('Error saving uploaded file to ' + path)
+                continue
+            else:
+                uploads.append(Upload(mime_type=upload.content_type, group=subdir, filename=name, path=path))
+            finally:
+                file_obj.close()
+        return compact('uploads')
