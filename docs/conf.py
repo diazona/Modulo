4a8343a36cbfdbfa566b5d48044a48bad697c1fa
David Zaslavsky <diazona@ellipsix.net>
2010-09-05 22:32:59 -0400

    Add autosummary extension and add cwd to sys.path for importing dummy settings module

--- docs/conf.py
+++ docs/conf.py
@@ -16,13 +16,17 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.append(os.path.abspath('..'))
+sys.path.extend([
+  os.path.abspath('..'),   # root of the Modulo source tree
+  os.path.abspath('.')     # modulo.database needs a settings.py to import;
+])                         # a dummy one is provided in the current directory
+
 
 # -- General configuration -----------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinx.ext.todo', 'sphinx.ext.coverage']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
