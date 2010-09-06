053845eb433d50e15fbdd03e1899904dbdc87438
David Zaslavsky <diazona@ellipsix.net>
2010-09-05 23:40:17 -0400

    Add explanatory comment to dummy settings.py file

--- docs/settings.py
+++ docs/settings.py
@@ -1,1 +1,8 @@
+# -*- coding: utf-8 -*-
+# This is just a dummy settings.py file. If you want to regenerate
+# the documentation, Sphinx will attempt to import all the automatically
+# documented modules, including modulo.database, which requires there
+# to be a settings.py file on the path.
+#
+# If you're not regenerating the documentation, you don't need this file.
 database_url = 'sqlite://'
