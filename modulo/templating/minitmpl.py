a5e8423a63815dd3be2f2009154d4851603af72f
David Zaslavsky <diazona@ellipsix.net>
2011-02-12 05:07:30 -0500

    Add string version of MiniTemplate

--- modulo/templating/minitmpl.py
+++ modulo/templating/minitmpl.py
@@ -1,5 +1,6 @@
 # -*- coding: iso-8859-1 -*-
 
+from modulo.actions import Action
 from modulo.actions.standard import FileResource
 from werkzeug.templates import Template
 
@@ -9,3 +10,14 @@ class MiniTemplate(FileResource):
         template_data = self.req.environ.copy()
         template_data.update(kwargs)
         rsp.data = Template.from_file(self.filename).render(template_data)
+        
+class MiniStringTemplate(Action):
+    namespace = '*'
+    @classmethod
+    def derive(cls, template, **kwargs):
+        return super(MiniStringTemplate, cls).derive(template=template, **kwargs)
+    def generate(self, rsp, **kwargs):
+        template_data = self.req.environ.copy()
+        template_data.update(kwargs)
+        rsp.data = Template(self.template).render(template_data)
+        
