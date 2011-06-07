3ec02ea8ad1c31e90134b5ebd1de3115cec35150
David Zaslavsky <diazona@ellipsix.net>
2011-06-06 22:00:32 -0400

    Add module docstring to modulo.database

--- modulo/database.py
+++ modulo/database.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+'''Currently this module just contains some initialization code having to do with
+database access. It doesn't have any classes or useful variables. It may be
+changed or moved in the future.'''
+
 from elixir import metadata
 from elixir.options import options_defaults
 
